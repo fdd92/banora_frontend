3915fbacd9360d87b5cd8b50d589e3b88a9119f7
howie <haia25675@gmail.com>
2019-01-15 16:35:00 +0800

    'change'

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template id="main-page">
+    <v-ons-page>
+        <v-ons-toolbar>
+            <div class="center">Title</div>
+        </v-ons-toolbar>
+
+        <p style="text-align: center">
+            <v-ons-button @click="$ons.notification.alert('Hello World!')">
+                Click me!
+            </v-ons-button>
+        </p>
+    </v-ons-page>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
+
+<script>
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+
+import VueOnsen from 'vue-onsenui'; // This imports 'onsenui', so no need to import it separately
+Vue.use(VueOnsen); // VueOnsen set here as plugin to VUE. Done automatically if a call to window.Vue exists in the startup code.
+
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+    render: h => h(App),
+}).$mount('#app')
